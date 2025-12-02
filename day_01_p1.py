8090ebc25e2defa8547d9631e3f4c8a550828f61
Chris Wehner <c.wehner@c.wehner-FJ25QP9G44>
2025-12-01 16:31:05 -0800

    Day 01

---
+++ day_01_p1.py
@@ -0,0 +1,56 @@
+import re
+from dataclasses import dataclass
+
+def get_data():
+    my_index = 0
+    turns = []
+    with open("day_01_input.txt", "r") as f:
+        data = [line.strip() for line in f]
+
+    @dataclass
+    class Turn:
+        dir: str
+        amount: int
+
+    for item in data:
+        m = re.match(r"(.)?(.*)", item)
+        dir, amount = m.groups()
+        my_index += 1
+        turns.append(Turn(dir=dir, amount=int(amount)))
+
+    return turns
+
+data = get_data()
+
+def get_zeros_and_next_position(cur, turn):
+    zeros = 0
+    if turn.dir=='R':
+        for movement in range(turn.amount):
+            if cur == 99:
+                cur = 0
+            else:
+                cur += 1
+            if cur == 0:
+                zeros += 1
+    elif turn.dir=='L':
+        for movement in range(turn.amount):
+            if cur == 0:
+                cur = 99
+            else:
+                cur -= 1
+            if cur == 0:
+                zeros += 1
+    return zeros, cur
+
+def count_zeros(data):
+    count_zeros = 0
+    cur = 50
+
+    for turn in data:
+        prev_zeros, cur = get_zeros_and_next_position(cur, turn)
+        count_zeros += prev_zeros
+
+    return count_zeros
+
+total = count_zeros(data)
+print(f'Total zeros = {total}')
