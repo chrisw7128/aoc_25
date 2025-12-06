cfebbbd6f6964a7610471c0e8b11799b0e54ad1f
Chris Wehner <c.wehner@c.wehner-FJ25QP9G44>
2025-12-05 16:51:45 -0800

    edited typos

--- day_05_p1.py
+++ day_05_p1.py
@@ -2,19 +2,8 @@ from dataclasses import dataclass
 
 with open("day_05_input.txt", "r") as f:
     first_section, second_section = f.read().split("\n\n", 1)
-
     fresh_ingredients = first_section.splitlines()
     available_ingredients = second_section.splitlines()
-
-    print(len(fresh_ingredients))
-    print(len(available_ingredients))
-    print(type(fresh_ingredients))
-    print(type(available_ingredients))
-    print(fresh_ingredients[0])
-    print(available_ingredients[0])
-    print(type(fresh_ingredients[0]))
-    print(type(available_ingredients[0]))
-
 
 @dataclass
 class Ranges:
@@ -27,17 +16,10 @@ def construct_fresh_ranges(fresh_ingredients):
         start = int(range.split('-')[0])
         end = int(range.split('-')[1])
         ranges.append(Ranges(start=start, end=end))
-
+        
     return ranges
 
 fresh_ingredients_ranges = construct_fresh_ranges(fresh_ingredients)
-
-print(type(fresh_ingredients_ranges))
-print(type(fresh_ingredients_ranges[0]))
-print(fresh_ingredients_ranges[0])
-print(type(fresh_ingredients_ranges[0].start))
-print(fresh_ingredients_ranges[0].start)
-print(type(available_ingredients[0]))
 
 def count_num_of_available_fresh_ingredients(available_ingredients, fresh_ingredients_ranges):
     fresh_total = 0
@@ -54,5 +36,48 @@ print(f'p1 fresh total: {fresh_total}')
 
 # p1 answer is 701
 
-######## Part 2 ########
+# ######## Part 2 ########
 
+# from dataclasses import dataclass
+
+# first_section = 0
+
+# with open("day_05_sample_p2.txt", "r") as f:
+#     try:
+#         first_section, second_section = f.read().split("\n\n", 1)
+#     except ValueError:
+#         with open("day_05_sample_p2.txt", "r") as f:
+#             ranges = f.readlines()
+
+#     if first_section:
+#         ranges = first_section.splitlines()
+
+
+#     print(len(ranges))
+#     print(type(ranges))
+#     print(ranges[1])
+#     print(type(ranges[0]))
+#     print(repr(ranges[1]))
+
+
+# @dataclass
+# class Ranges:
+#     start: int
+#     end: int
+
+# def construct_fresh_ranges(fresh_ingredients):
+#     ranges = []
+#     for range in fresh_ingredients:
+#         start = int(range.split('-')[0])
+#         end = int(range.split('-')[1])
+#         ranges.append(Ranges(start=start, end=end))
+
+#     return ranges
+
+# old_ranges = construct_fresh_ranges(ranges)
+
+# def combine_ranges(old_ranges):
+#     new_range_list = []
+#     for new_range in new_ranges:
+#         for old_range in old_ranges:
+#             # RIP
